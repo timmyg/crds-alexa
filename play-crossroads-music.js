3c0b8ef9772d6ad8cb0fe50b59f5f5965496474f
Tim Giblin <timmyg13@gmail.com>
2017-01-26 11:15:47 -0500

    wip

--- play-crossroads-music.js
+++ play-crossroads-music.js
@@ -1,7 +1,7 @@
 var https = require('https')
- 
+
 exports.handler = (event, context) => {
-  console.log('api key', process.env.STREAMSPOT_API_KEY)
+
   try {
 
     if (event.session.new) {
@@ -31,10 +31,28 @@ exports.handler = (event, context) => {
             console.log("Garbage...");
             throw "Invalid intent"
 
+          case "GetNextServiceTime":
+            var endpoint = "https://www.crossroads.net/proxy/content/api/SystemPage/?StateName=live"
+            var body = ""
+            https.get(endpoint, (response) => {
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+                var data = JSON.parse(body)
+                var streamTimes = data.systemPages[0].description
+                context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(streamTimes, true),
+                    {}
+                  )
+                )
+              })
+            })
+            break;
+
           case "PlayMusic":
             context.succeed(
               generateResponse(
-                buildSpeechletResponse(`I can't play music yet`, true),
+                buildSpeechletResponse('I cant play music yet'),
                 {}
               )
             )
@@ -76,38 +94,9 @@ buildSpeechletResponse = (outputText, shouldEndSession) => {
 generateResponse = (speechletResponse, sessionAttributes) => {
 
   return {
-    "version": "1.0",
-    "sessionAttributes": {},
-    "response": {
-      "outputSpeech": {
-        "type": "PlainText",
-        "text": "Playing the requested song."
-      },
-      "card": {
-        "type": "Simple",
-        "title": "Play Audio",
-        "content": "Playing the requested song."
-      },
-      "reprompt": {
-        "outputSpeech": {
-          "type": "PlainText",
-          "text": null
-        }
-      },
-      "directives": [
-        {
-          "type": "AudioPlayer.Play",
-          "playBehavior": "ENQUEUE",
-          "audioItem": {
-            "stream": {
-              "token": "this-is-the-audio-token",
-              "url": "https://s3.amazonaws.com/crds-cms-uploads/media/music/01-Whatever-Pleases-You-Army-Version.mp3",
-              "offsetInMilliseconds": 0
-            }
-          }
-        }
-      ],
-      "shouldEndSession": true
-    }
+    version: "1.0",
+    sessionAttributes: sessionAttributes,
+    response: speechletResponse
   }
+
 }
