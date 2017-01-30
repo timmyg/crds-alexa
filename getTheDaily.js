02fd43a9d671d463c96d3bf5d898cd234e8a5012
Tim Giblin <timmyg13@gmail.com>
2017-01-30 14:32:59 -0500

    callback stuff

--- getTheDaily.js
+++ getTheDaily.js
@@ -13,9 +13,10 @@ class GetTheDaily extends Skill{
     let date = moment(this.request.intent.slots.FromDate.value).format("YYYY-MM-DD")
     this.context.succeed(
       this.generateResponse(
-        this.getTheDailyText(date, (text) =>
-          this.buildSpeechletResponse(text, true),
-          {}
+        this.getTheDailyText(date, (text) => {
+            this.buildSpeechletResponse(text, true),
+            {}
+          }
         )
       )
     );
