76a3d54c71e32ea48ad98bc886748d3ff8caedef
Tim Giblin <timmyg13@gmail.com>
2017-01-31 13:20:59 -0500

    hours

--- skills/getLocationServiceTimes/getLocationServiceTimes.js
+++ skills/getLocationServiceTimes/getLocationServiceTimes.js
@@ -19,10 +19,37 @@ class GetLocationServiceTimes extends Skill {
   }
 
   getTimesString(serviceLocation) {
+    if (serviceLocation == 'East Side' || serviceLocation == 'Columbus') {
+      return `${serviceLocation} is not yet open. Check back soon!`
+    }
     let str = [`Service times at ${serviceLocation} are`];
     switch (serviceLocation) {
       case 'Oakley':
         str.push('Saturdays at 4:30 and 6:15pm and Sundays at 8:30am, 10:05am & 11:45am');
+        break;
+      case 'Mason':
+        str.push('Saturdays at 5pm and Sundays at 9:15am, 11am');
+        break;
+      case 'Oxford':
+        str.push('Sundays at 11am');
+        break;
+      case 'Clifton':
+        str.push('Sundays at 11am');
+        break;
+      case 'West Side':
+        str.push('Saturdays at 5:30pm and Sundays at 9:15am & 11am');
+        break;
+      case 'Andover':
+        str.push('Sundays at 9:15am & 11am');
+        break;
+      case 'Florence':
+        str.push('Saturdays at 5:30 and Sundays at 9:30am & 11:30am');
+        break;
+      case 'Gerogetown':
+        str.push('Sundays at 9:15am & 11:00am');
+        break;
+      case 'Richmond':
+        str.push('Sundays at 9:15am & 11:00am');
         break;
     }
     return str.join(' ');
