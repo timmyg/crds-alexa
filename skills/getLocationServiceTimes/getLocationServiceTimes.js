6ba9e693636eb7717a839ba0268bec930bc74f0d
Tim Giblin <timmyg13@gmail.com>
2017-01-31 13:08:39 -0500

    oakley times

--- skills/getLocationServiceTimes/getLocationServiceTimes.js
+++ skills/getLocationServiceTimes/getLocationServiceTimes.js
@@ -11,10 +11,21 @@ class GetLocationServiceTimes extends Skill {
   execute() {
     const serviceLocation = this.request.intent.slots.ServiceLocation.value
     if (serviceLocation) {
+        const timesString = this.getTimesString(serviceLocation);
         this.context.succeed(this.generateResponse(this.buildSpeechletResponse(`The hours at ${serviceLocation} are blahblah`, true), {}));
     } else {
         this.context.succeed(this.generateResponse(this.buildSpeechletResponse(`At which location?`, false, false, `At which location do you want service times? We have locations in Oakley, Mason, Oxford, Clifton, West Side, East Side, Columbus, Andover, Florence, Gerogetown, and Richmond`), {intent: 'GetLocationServiceTimes'}));
     }
+  }
+
+  getTimesString(serviceLocation) {
+    let str = [`Service times at ${serviceLocation} are`];
+    switch (serviceLocation) {
+      case 'Oakley':
+        str.push('Saturdays at 4:30 and 6:15pm and Sundays at 8:30am, 10:05am & 11:45am');
+        break;
+    }
+    return str.join(' ');
   }
 }
 
