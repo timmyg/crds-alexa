20c5c88594f2b1c981fbb9a9bcdae009c32481a9
Tim Giblin <timmyg13@gmail.com>
2017-06-20 12:34:58 -0400

    change err to results

--- skills/bestDrinker/bestDrinker.js
+++ skills/bestDrinker/bestDrinker.js
@@ -39,7 +39,7 @@ class BestDrinker extends Skill {
           callback(body)
         });
       }
-    ], function(err, results) {
+    ], function(results, err) {
       console.log("ERRR", err);
       console.log("RESULTS", results);
       // console.log(results[0].objects.length)
